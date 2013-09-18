0e289584f06d02cde6066b9ecb07731fdc60c1dd
Matthew Kramer <matthewkram@gmail.com>
2013-09-18 14:25:10 -0400

    finished decode huffman

--- week0101/decode.c
+++ week0101/decode.c
@@ -1,7 +1,58 @@
 #include <stdio.h>
+#include <strings.h>
 
 int main(int argc, char const *argv[])
 {
-	printf("This is a test\n");
+	FILE* in = fopen("decodeFIXED.txt", "r");
+	char arr[1000];
+	bzero(arr, 1000);
+	char ch;
+	int location;
+	for (int i = 0; i < 1000; ++i)
+	{
+		i[arr] = '*';
+	}
+	// arr[0] = '*';
+	char temp;
+	while(fread(&temp, 1, 1, in))
+	{
+		location = 1;
+		while(1)
+		{
+			fread(&ch, 1, 1, in);
+			if(ch != '\n')
+			{
+				if (ch == '0')
+				{
+					location *= 2;
+				}
+				else
+				{
+					location = location*2 + 1;
+				}
+			}
+			else
+			{
+				arr[location] = temp;
+				// printf("THIS IS A DEBUG STATEMENT\n");
+				break;
+			}		
+		}
+	}
+	FILE* message = fopen("message.txt", "r");
+	location = 1;
+	while(fread(&ch, 1, 1, message))
+	{
+		if(ch == '0')
+			location *= 2;
+		else
+			location = location*2 + 1;
+		if(arr[location] != '*')
+		{
+			printf("%c", arr[location]);
+			location = 1;
+		}
+	}
+	printf("\n");
 	return 0;
 }
